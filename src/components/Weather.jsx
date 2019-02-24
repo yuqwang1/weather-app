dabecdc48aea30ad5c869a9441e1ec03bb8786a9
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-02-23 21:55:16 -0500

    let the app render errors

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,12 +5,18 @@ class Weather extends Component {
   render() {
     const { temperature, city, humidity, description, country } = this.props
     return(
+      temperature === undefined ?
+      <h1>please enter a valid city and country</h1> :
       <div>
-        Temperature: { temperature },
+        Temperature: { temperature } Degree,
+        <br></br>
         Humidity: { humidity },
+        <br></br>
         City: { city },
+        <br></br>
         Country: { country },
-        Description: { description },
+        <br></br>
+        Condition: { description },
 
       </div>
     )
